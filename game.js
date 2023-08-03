7f7bb9a603947c570e9034b5057fb54f6746e262
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-08-03 21:30:12 +0100

    added playRound function with logic

--- game.js
+++ game.js
@@ -1,13 +1,59 @@
 function getComputerChoice() {
     let rand = Math.floor(Math.random()*3);
     switch(rand){
-        case 0: return "Rock";
+        case 0: return "rock";
         break;
-        case 1: return "Paper";
+        case 1: return "paper";
         break;
-        case 2: return "Scissors";
+        case 2: return "scissors";
         break;
         default: return "Error";
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection === "rock"){
+        if(computerSelection === "rock"){
+            return "Draw! Rock vs Rock";
+        } else if(computerSelection === "paper"){
+            return "You lost! Paper beats rock";
+        } else if(computerSelection === "scissors"){
+            return "You Won! Rock beats scissors";
+        } else {
+            throw new Error("ERROR");
+        }
+    }
+
+    if(playerSelection === "paper"){
+        if(computerSelection === "paper"){
+            return "Draw! Paper vs Paper";
+        } else if(computerSelection === "rock"){
+            return "You win! Paper beats rock";
+        } else if(computerSelection === "scissors"){
+            return "You Lost! Scissors beats paper";
+        } else {
+            throw new Error("ERROR");
+        }
+    }
+
+    if(playerSelection === "scissors"){
+        if(computerSelection === "scissors"){
+            return "Draw! scissors vs scissors";
+        } else if(computerSelection === "rock"){
+            return "You Lost! Rock beats Scissors";
+        } else if(computerSelection === "paper"){
+            return "You Won! Scissors beats paper";
+        } else {
+            throw new Error("ERROR");
+        }
+    }
+
+}
+
+comp = getComputerChoice();
+player = prompt("Enter rock paper or scissors");
+console.log(comp);
+console.log(player);
+console.log(playRound(player,comp));
