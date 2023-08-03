4144c1793d239cebd8e41a620c3441b2dccfce5c
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-08-03 21:15:14 +0100

    added function to generate random computer choice

---
+++ game.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let rand = Math.floor(Math.random()*3);
+    switch(rand){
+        case 0: return "Rock";
+        break;
+        case 1: return "Paper";
+        break;
+        case 2: return "Scissors";
+        break;
+        default: return "Error";
+    }
+}
+
