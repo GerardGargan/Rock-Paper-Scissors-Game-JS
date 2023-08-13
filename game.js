4a409912d407579a51b86c37bffa559f2d29caae
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-08-13 21:21:59 +0100

    added visual images for current round player vs computer, added 5 rounds

--- game.js
+++ game.js
@@ -1,52 +1,66 @@
 function getComputerChoice() {
     let rand = Math.floor(Math.random()*3);
     switch(rand){
-        case 0: return "rock";
+        case 0: 
+        compChoiceDiv.innerHTML = `<img src="./resources/rock.png">`;
+        return "rock";
         break;
-        case 1: return "paper";
+        case 1: 
+        compChoiceDiv.innerHTML = `<img src="./resources/paper.png">`;
+        return "paper";
         break;
-        case 2: return "scissors";
+        case 2: 
+        compChoiceDiv.innerHTML = `<img src="./resources/scissors.png">`;
+        return "scissors";
         break;
-        default: return "Error";
+        default: 
+        compChoiceDiv.innerHTML = ``;
+        return "Error";
     }
 }
 
 function playRound(playerSelection){
     playerSelection = playerSelection.toLowerCase();
     let computerSelection = getComputerChoice();
+    let result = "";
 
+    if(playerScore<5 && compScore<5){
+
+   
 
 
     if(playerSelection === "rock"){
         if(computerSelection === "rock"){
-            return "Draw! Rock vs Rock";
+            result = "Draw! Rock vs Rock";
         } else if(computerSelection === "paper"){
             compScore++;
             compScoreDiv.innerText = compScore;
-            return "Player lost! Computer Won! Paper beats rock";
+            result = "Player lost! Computer Won! Paper beats rock";
             
         } else if(computerSelection === "scissors"){
             playerScore++;
             playerScoreDiv.innerText = playerScore;
-            return "Player Won! Computer lost! Rock beats scissors";
+            result = "Player Won! Computer lost! Rock beats scissors";
             
         } else {
             throw new Error("ERROR");
         }
+
+
     }
 
     if(playerSelection === "paper"){
         if(computerSelection === "paper"){
-            return "Draw! Paper vs Paper";
+            result = "Draw! Paper vs Paper";
         } else if(computerSelection === "rock"){
             playerScore++;
             playerScoreDiv.innerText = playerScore;
-            return "Player won! Computer lost! Paper beats rock";
+            result = "Player won! Computer lost! Paper beats rock";
             
         } else if(computerSelection === "scissors"){
             compScore++;
             compScoreDiv.innerText = compScore;
-            return "Player Lost! Computer Wins! Scissors beats paper";
+            result = "Player Lost! Computer Wins! Scissors beats paper";
             
         } else {
             throw new Error("ERROR");
@@ -55,49 +69,59 @@ function playRound(playerSelection){
 
     if(playerSelection === "scissors"){
         if(computerSelection === "scissors"){
-            return "Draw! scissors vs scissors";
+            result = "Draw! scissors vs scissors";
         } else if(computerSelection === "rock"){
             compScore++;
             compScoreDiv.innerText = compScore;
-            return "Player Lost! Computer won! Rock beats Scissors";
+            result = "Player Lost! Computer won! Rock beats Scissors";
             
         } else if(computerSelection === "paper"){
             playerScore++;
             playerScoreDiv.innerText = playerScore;
-            return "Player Won! Computer Lost! Scissors beats paper";
+            result = "Player Won! Computer Lost! Scissors beats paper";
             
         } else {
             throw new Error("ERROR");
         }
     }
 
-    
+    resultDiv.innerText = result;
+
+} else {
+        const winner = playerScore > compScore ? "Player wins!" : "Computer Wins!";
+        overallResultDiv.textContent = winner;
+    }       
 
 }
 
 let playerScore = 0;
 let compScore = 0;
 
-const resultsDiv = document.querySelector(".gameResult");
-const playerDiv = document.querySelector(".playerMove");
-const compDiv = document.querySelector(".compMove");
+
 const playerScoreDiv = document.querySelector(".playerScore");
 const compScoreDiv = document.querySelector(".compScore");
-const currentRoundDiv = document.querySelector(".currentRound");
+
+const playersChoiceDiv = document.querySelector(".playerChoiceImg");
+const compChoiceDiv = document.querySelector(".compChoiceImg");
+const resultDiv = document.querySelector(".roundResults");
 
 const rockButton = document.querySelector("#rock");
+const overallResultDiv = document.querySelector(".overallResult");
 
 rockButton.addEventListener("click", (e) => {
-    currentRoundDiv.innerText = playRound("rock");
+    playRound("rock");
+    playersChoiceDiv.innerHTML = `<img src="/resources/rock.png">`;
 });
 
 const paperButton = document.querySelector("#paper");
 paperButton.addEventListener("click", (e) => {
-    currentRoundDiv.innerText = playRound("paper");
+    playRound("paper");
+    playersChoiceDiv.innerHTML = `<img src="./resources/paper.png">`;
 });
 
 const scissorButton = document.querySelector("#scissors");
 scissorButton.addEventListener("click", (e) =>{
-    currentRoundDiv.innerText = playRound("scissors");
+    playRound("scissors");
+    playersChoiceDiv.innerHTML = `<img src="./resources/scissors.png">`;
 })
 
